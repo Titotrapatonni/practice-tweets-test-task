110f27f6834199a7ece64b69d6594de5ae08bbc8
Titotrapatonni <titovtelexfree@gmail.com>
2023-04-26 22:50:31 +0300

    change path

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tweets" element={<Tweets />} />
-        <Route path="/*" element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </div>
   );
